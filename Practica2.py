a9974a36dae88c16d5a21abdbb6843ea84eb2260
valadezzz <eliud12isai@gmail.com>
2026-05-03 16:58:44 -0600

    método de mayor

--- Practica2.py
+++ Practica2.py
@@ -7,4 +7,7 @@ class Mi_Clase:
     def sumar(self):
         return self.__num1 + self.__num2 + self.__num3
     
+    def mayor(self):
+        return max(self.__num1, self.__num2, self.__num3)
+    
     
