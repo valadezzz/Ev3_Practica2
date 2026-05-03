1c691c525286f2f86b5cd86b6314fef2d3e9ec78
valadezzz <eliud12isai@gmail.com>
2026-05-03 17:04:51 -0600

    método concatenar

--- Practica2.py
+++ Practica2.py
@@ -16,4 +16,7 @@ class Mi_Clase:
     def iguales(self):
         return self.__num1 == self.__num2 == self.__num3
     
+    def concatenar(self):
+        return str(self.__num1) + str(self.__num2) + str(self.__num3)
+    
     
