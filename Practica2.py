6bdf6e6cc84aeb5993bf3e306cb742ab1805cf07
valadezzz <eliud12isai@gmail.com>
2026-05-03 17:02:35 -0600

    método iguales

--- Practica2.py
+++ Practica2.py
@@ -13,3 +13,7 @@ class Mi_Clase:
     def menor(self):
         return min(self.__num1, self.__num2, self.__num3)
     
+    def iguales(self):
+        return self.__num1 == self.__num2 == self.__num3
+    
+    
